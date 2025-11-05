70b0809f21d172cff581b217fcc8421bc13cb433
Balu <vknbalu111@gmail.com>
2025-11-05 22:17:08 +0530

    First commit of basic web

---
+++ src/main/java/com/app/basicweb/WebController.java
@@ -0,0 +1,12 @@
+package com.app.basicweb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+    @GetMapping("/")
+    public String hello(){
+        return "Hello";
+    }
+}
